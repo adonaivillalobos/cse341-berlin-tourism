f73a3afb447937cd3517fc31df7adf2ef1517bb7
Adonai Villalobos <adonaivillalobos31@gmail.com>
2026-07-07 18:53:51 -0600

    auth

--- auth.js
+++ auth.js
@@ -1,12 +1,16 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const callbackURL = process.env.NODE_ENV === 'production'
+  ? 'https://cse341-berlin-tourism.onrender.com/auth/google/callback'
+  : 'http://localhost:8080/auth/google/callback';
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback'
+      callbackURL: callbackURL
     },
     (accessToken, refreshToken, profile, done) => {
       return done(null, profile);
